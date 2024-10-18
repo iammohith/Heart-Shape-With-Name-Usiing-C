c47aeb4476b5471f8b8e91d34996fb4549129b9b
Mohith Sai Gorla <46181469+iammohith@users.noreply.github.com>
2024-10-18 22:57:06 +0530

    Update heartshapewithname.c

--- heartshapewithname.c
+++ heartshapewithname.c
@@ -1,61 +1,75 @@
 #include <stdio.h>
 #include <string.h>
-int main()
-{
-    int i, j, n;
-    char name[50];
-    int len;
-    printf("Enter your name:");
-    gets(name);
-    printf("Enter value of n:");
-    scanf("%d", &n);
-    len = strlen(name);
-    for(i=n/2; i<=n; i+=2)
-    {
-        for(j=1; j<n-i; j+=2)
-        {
+
+int main() {
+    int i, j, n;  // Loop counters and input size variable
+    char name[50];  // Array to store the user's name
+    int len;  // Variable to store the length of the name
+
+    // Input: Get the name and size from the user
+    printf("Enter your name: ");
+    gets(name);  // Get the name (unsafe, better to use fgets for larger programs)
+    
+    printf("Enter value of n: ");
+    scanf("%d", &n);  // Get the value of 'n', which controls the size of the heart shape
+
+    len = strlen(name);  // Get the length of the name
+
+    // Top half of the heart
+    for (i = n / 2; i <= n; i += 2) {
+        // Print leading spaces before the left half of the heart
+        for (j = 1; j < n - i; j += 2) {
             printf(" ");
         }
-        for(j=1; j<=i; j++)
-        {
+        
+        // Print the left half of the heart
+        for (j = 1; j <= i; j++) {
             printf("*");
         }
-        for(j=1; j<=n-i; j++)
-        {
+        
+        // Print spaces between the two halves of the heart
+        for (j = 1; j <= n - i; j++) {
             printf(" ");
         }
-        for(j=1; j<=i; j++)
-        {
+        
+        // Print the right half of the heart
+        for (j = 1; j <= i; j++) {
             printf("*");
         }
-        printf("\n");
+        
+        printf("\n");  // Move to the next line
     }
-    for(i=n; i>=1; i--)
-    {
-        for(j=i; j<n; j++)
-        {
+
+    // Bottom half of the heart, where the name will be printed
+    for (i = n; i >= 1; i--) {
+        // Print leading spaces
+        for (j = i; j < n; j++) {
             printf(" ");
         }
-        if(i == n) 
-        {
-            for(j=1; j<=(n * 2-len)/2; j++)
-            {
+        
+        // Print the middle line containing the name
+        if (i == n) {
+            // Print stars before the name
+            for (j = 1; j <= (n * 2 - len) / 2; j++) {
                 printf("*");
-            }   
+            }
+
+            // Print the user's name in the middle
             printf("%s", name);
-            for(j=1; j<(n*2-len)/2; j++)
-            {
+
+            // Print stars after the name
+            for (j = 1; j < (n * 2 - len) / 2; j++) {
+                printf("*");
+            }
+        } else {
+            // Print stars for the other rows of the bottom half
+            for (j = 1; j <= (i * 2) - 1; j++) {
                 printf("*");
             }
         }
-        else 
-        {
-            for(j=1; j<=(i*2)-1; j++)
-            {
-                printf("*");
-            }
-        }
-        printf("\n");
+
+        printf("\n");  // Move to the next line
     }
-    return 0;
+
+    return 0;  // End of program
 }
